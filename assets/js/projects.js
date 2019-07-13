ae31cacf9bb2d6ea046da6f48aaccfce92f1edc1
s <baidu58@yeah.net>
2019-07-13 11:05:53 +0800

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,41 +1,5 @@
 var DEMO = (function(win,$){
-	var JSON = [{
-		'name' : '戴尔',
-		'computer' : {
-			'name' : 'XPS 13 触控超极本',
-			'stock_num' : 5000,
-			'price' : 8000,
-			'channel' : '太平洋大厦<a href="http://www.baidu.com" title = "点击进入百度">百度</a>',
-			'detail' : {
-				'name' : '睿智公司',
-				'address' : '5-08号'
-			}
-		}
-	},{
-		'name' : 'Thinkpad',
-		'computer' : {
-			'name' : 'ThinkPad T440（20B6S00200）',
-			'stock_num' : 3000,
-			'price' : 7500,
-			'channel' : '海龙大厦',
-			'detail' : {
-				'name' : '倾城公司',
-				'address' : '3-12号'
-			}
-		}
-	},{
-		'name' : '苹果',
-		'computer' : {
-			'name' : '苹果MacBook Pro（ME865CH/A）',
-			'stock_num' : 2000,
-			'price' : 9000,
-			'channel' : '海龙大厦',
-			'detail' : {
-				'name' : '名扬公司',
-				'address' : '16-03号'
-			}
-		}
-	}],
+	
 	itemReg = /\$\w+[.]?\w+\$/gi,
 	detailReg = /\$\w+[.]?\w+[.]?\w+\$/gi;
 	String.prototype.temp = function(reg, obj) {
